1db78b0a28d15940df52221b8ee63a7d5ea13339
DadAquino <david.aquino.exeutivo@gmail.com>
2023-06-01 17:56:52 -0300

    conexão com o banco de dados estabelecida

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const connection = require('./connection');
 
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get('/', async (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
 
-module.exports = app;
+app.get('/test', async (_request, response) => {
+ const [result] = await connection.execute('SELECT * FROM StoreManager.products');
 
-// vamos começar, uhu!
+  return response.status(200).json({ result });
+});
+
+module.exports = app;
