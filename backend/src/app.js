ade14ec07ff33452d17bad2bad82b5764f642b02
DadAquino <david.aquino.exeutivo@gmail.com>
2023-06-13 17:17:18 -0300

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.json({ status: 'Store Manager UP!' });
-});
+    response.json({ status: 'Store Manager UP!' });
+  });
 
 app.get('/products', productsController.getProducts);
 app.get('/products/:id', productsController.getProductsById);
